1acd1b10258efead17d801444b5f26e028776d35
Boubacar DIENE <boubacar.diene@gmail.com>
2018-03-17 18:26:39 +0100

    [CONFIG] mmstreamer now distributed under GPLv2

--- inc/control/Controllers.h
+++ inc/control/Controllers.h
@@ -6,7 +6,7 @@
 //                                                                                              //
 //              mmstreamer is free software: you can redistribute it and/or modify              //
 //              it under the terms of the GNU General Public License as published by            //
-//              the Free Software Foundation, either version 3 of the License, or               //
+//              the Free Software Foundation, either version 2 of the License, or               //
 //              (at your option) any later version.                                             //
 //                                                                                              //
 //              mmstreamer is distributed in the hope that it will be useful,                   //
@@ -15,9 +15,11 @@
 //              GNU General Public License for more details.                                    //
 //                                                                                              //
 //              You should have received a copy of the GNU General Public License               //
-//              along with mmstreamer.  If not, see <http://www.gnu.org/licenses/>.             //
+//              along with mmstreamer. If not, see <http://www.gnu.org/licenses/>               //
+//              or write to the Free Software Foundation, Inc., 51 Franklin Street,             //
+//              51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.                   //
 //                                                                                              //
-//////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*!
 * \file Controllers.h
